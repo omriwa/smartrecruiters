788d2f4c4cdbe4d6bf865d1bab6539fbd66a921a
omri wallach <omriwallach1992@gmail.com>
2020-06-15 17:12:32 +0200

    create prompt

--- index.js
+++ index.js
@@ -1,15 +1,33 @@
 const prompt = require('prompt');
 const validator = /^-?\d*(\.\d+)?$/;
+const latitudeString = 'latitude';
+const longtitudeString = 'longtitude';
 const properties = [
     {
-        name: 'latitude',
+        name: latitudeString,
+        message: latitudeString + ':',
         validator,
         warning: 'Latitude should be float'
     },
     {
-        name: 'longtitude',
+        name: longtitudeString,
+        message: longtitudeString,
         validator,
         warning: 'Longtitude should be float'
     }
-]
+];
+
+// listenning to input
+console.log('Enter Latitude and Longtitude');
+prompt.start();
+// get input
+prompt.get(properties, (e, result) => {
+    if (e) {
+        console.log(e);
+        return 1;
+    } 
+    else {
+        console.log(result);
+    }
+});
 
